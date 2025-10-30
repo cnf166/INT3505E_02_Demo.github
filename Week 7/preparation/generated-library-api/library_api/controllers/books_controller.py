1f4847a8aa4717b5f1f38878696c794556a05682
wood <nguyenvietanh166.fw@gmail.com>
2025-10-30 18:11:26 +0700

    config controllers

--- Week 7/preparation/generated-library-api/library_api/controllers/books_controller.py
+++ Week 7/preparation/generated-library-api/library_api/controllers/books_controller.py
@@ -13,89 +13,163 @@ from library_api.models.get_books200_response import GetBooks200Response  # noqa
 from library_api import util
 
 
-def create_book(body):  # noqa: E501
-    """Create a new book
-
-    Tạo một cuốn sách mới # noqa: E501
-
-    :param book_create: 
-    :type book_create: dict | bytes
-
-    :rtype: Union[CreateBook201Response, Tuple[CreateBook201Response, int], Tuple[CreateBook201Response, int, Dict[str, str]]
-    """
-    book_create = body
-    if connexion.request.is_json:
-        book_create = BookCreate.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def delete_book(id):  # noqa: E501
-    """Delete a book
-
-    Xóa sách theo ID # noqa: E501
-
-    :param id: Book ID
-    :type id: int
-
-    :rtype: Union[DeleteBook200Response, Tuple[DeleteBook200Response, int], Tuple[DeleteBook200Response, int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_book_by_id(id):  # noqa: E501
-    """Get a book by id
-
-    Lấy thông tin sách theo ID # noqa: E501
-
-    :param id: Book ID
-    :type id: int
-
-    :rtype: Union[CreateBook201Response, Tuple[CreateBook201Response, int], Tuple[CreateBook201Response, int, Dict[str, str]]
-    """
-    return 'do some magic!'
-
-
-def get_books():  # noqa: E501
+def get_books():
     """Get all books
-
-    Lấy danh sách tất cả các sách # noqa: E501
-
-
-    :rtype: Union[GetBooks200Response, Tuple[GetBooks200Response, int], Tuple[GetBooks200Response, int, Dict[str, str]]
+    
+    :rtype: dict
     """
-    return 'do some magic!'
+    try:
+        books = BookDB.query.all()
+        books_list = [book.to_dict() for book in books]
+        return {'books': books_list}, 200
+    except Exception as e:
+        return {'message': str(e)}, 500
 
 
-def search_books(q=None, page=None, per_page=None):  # noqa: E501
+def create_book(body):
+    """Create a new book
+    
+    :param body: Book data
+    :type body: dict | bytes
+    
+    :rtype: dict
+    """
+    try:
+        if isinstance(body, dict):
+            title = body.get('title')
+            author = body.get('author')
+        else:
+            title = body.title
+            author = body.author
+        
+        if not title or not author:
+            return {'message': 'Title and author are required'}, 400
+        
+        new_book = BookDB(title=title, author=author)
+        db.session.add(new_book)
+        db.session.commit()
+        
+        return {'book': new_book.to_dict()}, 201
+    except Exception as e:
+        db.session.rollback()
+        return {'message': str(e)}, 500
+
+
+def get_book_by_id(id):
+    """Get a book by id
+    
+    :param id: Book ID
+    :type id: int
+    
+    :rtype: dict
+    """
+    try:
+        book = BookDB.query.get(id)
+        if not book:
+            return {'message': 'Book not found'}, 404
+        
+        return {'book': book.to_dict()}, 200
+    except Exception as e:
+        return {'message': str(e)}, 500
+
+
+def update_book(id, body):
+    """Update a book
+    
+    :param id: Book ID
+    :type id: int
+    :param body: Updated book data
+    :type body: dict | bytes
+    
+    :rtype: dict
+    """
+    try:
+        book = BookDB.query.get(id)
+        if not book:
+            return {'message': 'Book not found'}, 404
+        
+        if isinstance(body, dict):
+            title = body.get('title')
+            author = body.get('author')
+        else:
+            title = body.title
+            author = body.author
+        
+        if title:
+            book.title = title
+        if author:
+            book.author = author
+        
+        db.session.commit()
+        return {'book': book.to_dict()}, 200
+    except Exception as e:
+        db.session.rollback()
+        return {'message': str(e)}, 500
+
+
+def delete_book(id):
+    """Delete a book
+    
+    :param id: Book ID
+    :type id: int
+    
+    :rtype: dict
+    """
+    try:
+        book = BookDB.query.get(id)
+        if not book:
+            return {'message': 'Book not found'}, 404
+        
+        book_dict = book.to_dict()
+        db.session.delete(book)
+        db.session.commit()
+        
+        return {'book_deleted': book_dict}, 200
+    except Exception as e:
+        db.session.rollback()
+        return {'message': str(e)}, 500
+
+
+def search_books(q=None, page=None, per_page=None):
     """Search books by title or author with pagination
-
-    Tìm kiếm sách theo tiêu đề hoặc tác giả với phân trang # noqa: E501
-
+    
     :param q: Search query for title or author
     :type q: str
     :param page: Page number
     :type page: int
     :param per_page: Number of items per page
     :type per_page: int
-
-    :rtype: Union[BookSearchResponse, Tuple[BookSearchResponse, int], Tuple[BookSearchResponse, int, Dict[str, str]]
+    
+    :rtype: BookSearchResponse
     """
-    return 'do some magic!'
-
-
-def update_book(id, body):  # noqa: E501
-    """Update a book
-
-    Cập nhật thông tin sách # noqa: E501
-
-    :param id: Book ID
-    :type id: int
-    :param book_update: 
-    :type book_update: dict | bytes
-
-    :rtype: Union[CreateBook201Response, Tuple[CreateBook201Response, int], Tuple[CreateBook201Response, int, Dict[str, str]]
-    """
-    book_update = body
-    if connexion.request.is_json:
-        book_update = BookUpdate.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    try:
+        # Default values
+        search_query = q if q else ''
+        page = page if page else 1
+        per_page = per_page if per_page else 10
+        
+        query = BookDB.query
+        
+        if search_query:
+            search_pattern = f'%{search_query}%'
+            query = query.filter(
+                db.or_(
+                    BookDB.title.ilike(search_pattern),
+                    BookDB.author.ilike(search_pattern)
+                )
+            )
+        
+        paginated_books = query.paginate(page=page, per_page=per_page, error_out=False)
+        
+        books_list = [book.to_dict() for book in paginated_books.items]
+        
+        response = {
+            'books': books_list,
+            'total': paginated_books.total,
+            'pages': paginated_books.pages,
+            'current_page': paginated_books.page
+        }
+        
+        return response, 200
+    except Exception as e:
+        return {'message': str(e)}, 500
